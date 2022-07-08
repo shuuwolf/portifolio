430678c392ff0a3d1fac6565fc67cf688955e77c
Rafael Kauch <kauchrafael0001@gmail.com>
2022-07-08 14:55:59 -0300

    updating portifolio

--- js/functions.js
+++ js/functions.js
@@ -21,13 +21,11 @@
 //   ]
 // });
 
-$('.projeto-list').slick({
+$('.card-img-top').slick({
   dots: true,
   infinite: true,
-  speed: 1200,
   fade: true,
   cssEase: 'linear',
-  autoplay: true
 });
 
 $('.depoimentos').slick({
@@ -41,3 +39,28 @@ $('.depoimentos').slick({
 $("#trigger-mobile-menu").on('click', () => {
   $(".menu-mobile").slideToggle();
 })
+
+
+$(window).scroll( function(){
+/* Check the location of each desired element */
+    $('.hideme').each( function(i){
+  
+      var bottom_of_object = $(this).offset().top - 500 + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},2000);
+      }
+    }); 
+});
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top - 80
+    }, 500);
+  }
+});
